236556aee86866e67aeef38a4aaf6ab0543d6748
Tauqeer Khan <tauquir061@gmail.com>
2026-08-13 01:30:50 +0530

    Add config.js with hardcoded credentials
    Added hardcoded credentials for API, database, and JWT.

---
+++ config.js
@@ -0,0 +1,8 @@
+// Hardcoded credentials
+const config = {
+  apiKey: "sk-1234567890abcdefghijklmnopqrstuvwxyz",
+  dbPassword: "admin_password_123",
+  jwtSecret: "my-super-secret-jwt-key-12345"
+};
+
+module.exports = config;
